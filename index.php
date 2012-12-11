feea2c58cb0afcedfce737fbc3ae6de207a7098e
Colby Fayock <fayock@gmail.com>
2012-12-10 22:29:28 -0500

    dont think this is the right index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	$page = 'home';
+	include 'includes.php';
+?>
+<title>Frosty Deals - Bringing you the coldest one day deals of the web!</title>
+<?php
+	include 'header.php';
+	include 'deals.php';
+	include 'footer.php';
+?>
